1e735b24ca576f334d93b07bb94327701ff3e97f
AfsheenRashik <73888160+AfsheenRashik@users.noreply.github.com>
2021-01-05 20:47:44 +0530

    Add files via upload
    first

---
+++ box.js
@@ -0,0 +1,33 @@
+class Basket {
+
+
+    constructor(x,y,width,height){
+    
+        var  options={
+           isStatic :true
+         }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.height = height;
+    this.width = width;
+    
+    World.add(myWorld,this.body);
+
+    
+    
+    }
+    
+    
+    display(){
+    var pos = this.body.position
+    push();
+    rectMode(CENTER);
+    fill("red");
+    rect(pos.x,pos.y,this.width,this.height);
+    pop();
+    
+    
+    }
+    
+    
+    
+    }
